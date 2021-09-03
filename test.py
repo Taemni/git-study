2b81acb99e997a76b840983157294ac26c6d91fc
Taemni <stm3764@nate.com>
2021-09-04 01:33:21 +0900

    1

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import requests
 req = requests.get("https://iz-one.kr", headers={"User-Agent":"TEST"})
 
 if req.status_code != 200:
-    print("merge commit이 어떻게 ?d")
+    print("merge commit이 어떻게 ?")
